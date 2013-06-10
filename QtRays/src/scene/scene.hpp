19877b61ca56f61eacb9a53fdca216893501935a
John Richards <john@pushpoplabs.com>
2013-06-10 01:37:52 -0700

    Fleshed out Scene a wee bit.

--- QtRays/src/scene/scene.hpp
+++ QtRays/src/scene/scene.hpp
@@ -1,10 +1,16 @@
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
+#include "viewplane.hpp"
+#include "../util/rgbcolor.hpp"
+
 class Scene
 {
 public:
     Scene();
+
+    ViewPlane vp;
+    RGBColor bgcolor;
 };
 
 #endif // SCENE_HPP
