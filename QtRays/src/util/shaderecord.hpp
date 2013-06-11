997c885b00dba1331ef19d3115c35aa6700eef7b
John Richards <john@pushpoplabs.com>
2013-06-11 00:28:32 -0700

    Removed Scene data member from ShadeRecord.

--- QtRays/src/util/shaderecord.hpp
+++ QtRays/src/util/shaderecord.hpp
@@ -4,19 +4,17 @@
 #include "normal.hpp"
 #include "point3d.hpp"
 #include "rgbcolor.hpp"
-#include "../scene/scene.hpp"
 
 class ShadeRecord
 {
 public:
-    ShadeRecord(Scene& _w);
+    ShadeRecord();
     ShadeRecord(const ShadeRecord& sr);
 
     bool hit_object;
     Point3D local_hit_point;
     Normal n;
     RGBColor color;
-    Scene& w;
 };
 
 #endif // SHADERECORD_HPP
